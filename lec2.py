f4586e5e5cb231eef2db3f85881da911b450904d
EC2 Default User <ec2-user@ip-172-31-78-223.ec2.internal>
2022-02-16 17:42:42 +0000

    lab4 dict and tuple

---
+++ lec2.py
@@ -0,0 +1,10 @@
+#print('Hello World'.upper() )
+#print ('Hello World'.lower() )
+#print (' Hello World ')
+#print ('Hello Word'.split())
+#print ('Hello, World'.split('o'))
+#print(2*3)
+
+my_int = 123
+print(my_int)
+
